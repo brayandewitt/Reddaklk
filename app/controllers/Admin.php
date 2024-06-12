e3e9ac1c0af6bb0e8ee767cadebc95aa1de1f805
brayandewitt <brayandewitt56@gmail.com>
2024-06-12 22:29:36 +0530

    add profile image to databse

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -6,7 +6,7 @@ class Admin extends Controller
     public function index()
     {
 
-        if(!Auth::logged_in()){
+        if (!Auth::logged_in()) {
             message('please login to view admin section');
             redirect('login');
         }
@@ -15,7 +15,7 @@ class Admin extends Controller
     }
     public function profile($id = null)
     {
-        if(!Auth::logged_in()){
+        if (!Auth::logged_in()) {
             message('please login to view admin section');
             redirect('login');
         }
@@ -23,11 +23,33 @@ class Admin extends Controller
         $id = $id ?? Auth::getId();
 
         $user = new User();
-        $data['row'] = $row = $user->first(['id'=>$id]);
+        $data['row'] = $row = $user->first(['id' => $id]);
 
-        if($_SERVER['REQUEST_METHOD'] == "POST" && $row){
+        if ($_SERVER['REQUEST_METHOD'] == "POST" && $row) {
+
+            $folder = "uploads/images/";
+            if (!file_exists($folder)) {
+                mkdir($folder, 0777, true);
+                file_put_contents($folder . "index.php", "<?php //silence");
+                file_put_contents("uploads/index.php", "<?php //silence");
+            }
+
+            $allowed = ['image/jpeg', 'image/png'];
+            if (!empty($_FILES['image']['name'])) {
+                if ($_FILES['image']['error'] == 0) {
+                    if (in_array($_FILES['image']['type'], $allowed)) {
+
+                        $destination = $folder.time().$_FILES['image']['name'];
+                        move_uploaded_file($_FILES['image']['tmp_name'], $destination);
+                    } else {
+                        $user->errors['image'] = "This file type not allowed";
+                    }
+                } else {
+                    $user->errors['image'] = "Could not uploada image";
+                }
+            }
             $user->update($id, $_POST);
-            redirect('admin/profile/'.$id);
+            redirect('admin/profile/' . $id);
         }
 
         $data['tittle'] = "Profile";
