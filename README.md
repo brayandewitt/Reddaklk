ec51fb20f3dfe8abcc4675d187aca5669f0120ab
brayandewitt <brayandewitt56@gmail.com>
2024-06-14 00:30:11 +0530

    setup gitignorefiles and enviroment varibales

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Reddaklk
 cloth Store
+
+#Setup
+
+create .env file and copy .env.examples file data and add your password
--- app/core/init.php
+++ app/core/init.php
@@ -4,9 +4,9 @@ spl_autoload_register(function($class_name){
 });
 
 
-
-require"../app/core/config.php";
+require_once __DIR__."/../../vendor/autoload.php";
 require"../app/core/functions.php";
+require"../app/core/config.php";
 require"../app/core/database.php";
 require"../app/core/model.php";
 require"../app/core/controller.php";
