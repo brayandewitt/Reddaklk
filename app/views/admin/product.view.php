4a17f3f912ff6555134de778c16da8f59a7818e2
brayandewitt <brayandewitt56@gmail.com>
2024-06-20 06:52:33 +0530

    detect input

--- app/views/admin/product.view.php
+++ app/views/admin/product.view.php
@@ -38,55 +38,59 @@
 
 <?php elseif ($action == 'edit') : ?>
   <div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Edit Product</h5>
-    <?php if (!empty($row)) : ?>
-      <div class="float-end ">
-        <button class="btn btn-success">save</button>
-        <a href="<?=ROOT?>/admin/product">
-        <button class="btn btn-primary">back</button>
-        </a>
-      </div>
-      
-      <h5 class=""><?=esc($row->title)?></h5>
-      <!-- Bordered Tabs Justified -->
-      <ul class="nav nav-tabs nav-tabs-bordered d-flex" id="borderedTabJustified" role="tablist">
-        <li class="nav-item flex-fill" role="presentation">
-          <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100 active" id="productdetails-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-productdetails" type="button" role="tab" aria-controls="productdetails" aria-selected="true">Product details</button>
-        </li>
-        <li class="nav-item flex-fill" role="presentation">
-          <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100" id="price-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-price" type="button" role="tab" aria-controls="price" aria-selected="false">Price</button>
-        </li>
-        <li class="nav-item flex-fill" role="presentation">
-          <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100" id="images-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-images" type="button" role="tab" aria-controls="images" aria-selected="false">Images</button>
-        </li>
-        <li class="nav-item flex-fill" role="presentation">
-          <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100" id="promotions-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-promotions" type="button" role="tab" aria-controls="promotions" aria-selected="false">Promotion</button>
-        </li>
-      </ul>
-      <div class="tab-content pt-2" id="borderedTabJustifiedContent">
-        <div class="tab-pane fade show active" id="bordered-justified-productdetails" role="tabpanel" aria-labelledby="productdetails-tab">
-          1Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.
+    <div class="card-body">
+      <h5 class="card-title">Edit Product</h5>
+      <?php if (!empty($row)) : ?>
+        <div class="float-end ">
+          <button class="btn btn-success">save</button>
+          <a href="<?= ROOT ?>/admin/product">
+            <button class="btn btn-primary">back</button>
+          </a>
         </div>
-        <div class="tab-pane fade" id="bordered-justified-price" role="tabpanel" aria-labelledby="price-tab">
-          2Nesciunt totam et. Consequuntur magnam aliquid eos nulla dolor iure eos quia. Accusantium distinctio omnis et atque fugiat. Itaque doloremque aliquid sint quasi quia distinctio similique. Voluptate nihil recusandae mollitia dolores. Ut laboriosam voluptatum dicta.
+
+        <h5 class=""><?= esc($row->title) ?></h5>
+        <!-- Bordered Tabs Justified -->
+        <ul class="nav nav-tabs nav-tabs-bordered d-flex" id="borderedTabJustified" role="tablist">
+          <li class="nav-item flex-fill" role="presentation">
+            <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100 active" id="productdetails-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-productdetails" type="button" role="tab" aria-controls="productdetails" aria-selected="true">Product details</button>
+          </li>
+          <li class="nav-item flex-fill" role="presentation">
+            <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100" id="price-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-price" type="button" role="tab" aria-controls="price" aria-selected="false">Price</button>
+          </li>
+          <li class="nav-item flex-fill" role="presentation">
+            <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100" id="images-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-images" type="button" role="tab" aria-controls="images" aria-selected="false">Images</button>
+          </li>
+          <li class="nav-item flex-fill" role="presentation">
+            <button onclick="set_tab(this.getAttribute('data-bs-target'))" class="nav-link w-100" id="promotions-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-promotions" type="button" role="tab" aria-controls="promotions" aria-selected="false">Promotion</button>
+          </li>
+        </ul>
+        <div oninput="something_changed(event)" class="tab-content pt-2" id="borderedTabJustifiedContent">
+          <div class="tab-pane fade show active" id="bordered-justified-productdetails" role="tabpanel" aria-labelledby="productdetails-tab">
+            1Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.
+            <input type="text" name="">
+          </div>
+          <div class="tab-pane fade" id="bordered-justified-price" role="tabpanel" aria-labelledby="price-tab">
+            2Nesciunt totam et. Consequuntur magnam aliquid eos nulla dolor iure eos quia. Accusantium distinctio omnis et atque fugiat. Itaque doloremque aliquid sint quasi quia distinctio similique. Voluptate nihil recusandae mollitia dolores. Ut laboriosam voluptatum dicta.
+            <input type="text" name="">
+          </div>
+          <div class="tab-pane fade" id="bordered-justified-images" role="tabpanel" aria-labelledby="images-tab">
+            3Saepe animi et soluta ad odit soluta sunt. Nihil quos omnis animi debitis cumque. Accusantium quibusdam perspiciatis qui qui omnis magnam. Officiis accusamus impedit molestias nostrum veniam. Qui amet ipsum iure. Dignissimos fuga tempore dolor.
+            <input type="text" name="">
+          </div>
+          <div class="tab-pane fade" id="bordered-justified-promotions" role="tabpanel" aria-labelledby="promotions-tab">
+            4Saepe animi et soluta ad odit soluta sunt. Nihil quos omnis animi debitis cumque. Accusantium quibusdam perspiciatis qui qui omnis magnam. Officiis accusamus impedit molestias nostrum veniam. Qui amet ipsum iure. Dignissimos fuga tempore dolor.
+            <input type="text" name="">
+          </div>
         </div>
-        <div class="tab-pane fade" id="bordered-justified-images" role="tabpanel" aria-labelledby="images-tab">
-          3Saepe animi et soluta ad odit soluta sunt. Nihil quos omnis animi debitis cumque. Accusantium quibusdam perspiciatis qui qui omnis magnam. Officiis accusamus impedit molestias nostrum veniam. Qui amet ipsum iure. Dignissimos fuga tempore dolor.
+        <!-- End Bordered Tabs Justified -->
+      <?php else : ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <i class="bi bi-exclamation-octagon me-1"></i>
+          That product not found!
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        <div class="tab-pane fade" id="bordered-justified-promotions" role="tabpanel" aria-labelledby="promotions-tab">
-          4Saepe animi et soluta ad odit soluta sunt. Nihil quos omnis animi debitis cumque. Accusantium quibusdam perspiciatis qui qui omnis magnam. Officiis accusamus impedit molestias nostrum veniam. Qui amet ipsum iure. Dignissimos fuga tempore dolor.
-        </div>
-      </div>
-      <!-- End Bordered Tabs Justified -->
-    <?php else : ?>
-      <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <i class="bi bi-exclamation-octagon me-1"></i>
-        That product not found!
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    <?php endif ?>
-  </div>
+      <?php endif ?>
+    </div>
   </div>
 <?php else : ?>
 
@@ -150,20 +154,37 @@
 
 <script>
   var tab = sessionStorage.getItem("tab") ? sessionStorage.getItem("tab") : "#productdetails-tab";
+  var dirty = false;
 
 
+  function show_tab(tab_name) {
+    const someTabTriggerEl = document.querySelector(tab_name + "-tab");
+    const tab = new bootstrap.Tab(someTabTriggerEl);
+    tab.show();
+  }
 
-function show_tab(tab_name) {
-  const someTabTriggerEl = document.querySelector(tab_name + "-tab");
-  const tab = new bootstrap.Tab(someTabTriggerEl);
-  tab.show();
-}
+  function set_tab(tab_name) {
+    tab = tab_name;
+    sessionStorage.setItem("tab", tab_name);
 
-function set_tab(tab_name) {
-  tab = tab_name;
-  sessionStorage.setItem("tab", tab_name);
-  
-}
+    if (dirty) {
+      if (!confirm("Your changers  were not saaved. continue!")) {
+        tab = dirty;
+        sessionStorage.setItem("tab", dirty);
+        setTimeout(function() {
+          show_tab(dirty);
+        }, 10);
+
+      } else {
+        dirty = false;
+      }
+    }
+
+  }
+
+  function something_changed(e) {
+    dirty = tab;
+  }
 </script>
 
 <?php $this->view('admin/admin-footer', $data) ?>
